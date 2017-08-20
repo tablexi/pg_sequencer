e3425b9dde61c137b87aff8e49d079c58c037c57
Ben Linton <ben.linton@tablexi.com>
2017-08-20 00:40:33 -0500

    Refactor for ruby 2.0+ and rails 5.0+

---
+++ spec/pg_sequencer/schema_dumper_spec.rb
@@ -0,0 +1,70 @@
+require "spec_helper"
+require "support/active_record_mocks"
+
+describe PgSequencer::SchemaDumper do
+
+  let(:stream) { MockStream.new }
+  let(:connection) { MockConnection.new(sequences) }
+  let(:sequences) do
+    ["user_seq", "item_seq"].map do |name|
+      PgSequencer::SequenceDefinition.new(name, options)
+    end
+  end
+
+  context "with all options" do
+    let(:options) do
+      {
+        increment: 1,
+        min: 1,
+        max: 2_000_000,
+        start: 1,
+        cache: 5,
+        cycle: true,
+        owned_by: "table_name.column_name",
+      }
+    end
+
+    it "outputs all sequences correctly" do
+      expected_output = <<~SCHEMAEND
+                        # Fake Schema Header
+                        # (No Tables)
+                          create_sequence "item_seq", increment: 1, min: 1, max: 2000000, start: 1, cache: 5, cycle: true, owned_by: "table_name.column_name"
+                          create_sequence "user_seq", increment: 1, min: 1, max: 2000000, start: 1, cache: 5, cycle: true, owned_by: "table_name.column_name"
+
+                        # Fake Schema Trailer
+                        SCHEMAEND
+
+      MockSchemaDumper.dump(connection, stream)
+      expect(expected_output.strip).to eq(stream.to_s)
+    end
+  end
+
+  context "when min specified as false" do
+    let(:options) do
+      {
+        increment: 1,
+        min: false,
+        max: 2_000_000,
+        start: 1,
+        cache: 5,
+        cycle: true,
+        owned_by: "table_name.column_name",
+      }
+    end
+
+    it "outputs false for schema output" do
+      expected_output = <<~SCHEMAEND
+                        # Fake Schema Header
+                        # (No Tables)
+                          create_sequence "item_seq", increment: 1, min: false, max: 2000000, start: 1, cache: 5, cycle: true, owned_by: "table_name.column_name"
+                          create_sequence "user_seq", increment: 1, min: false, max: 2000000, start: 1, cache: 5, cycle: true, owned_by: "table_name.column_name"
+
+                        # Fake Schema Trailer
+                        SCHEMAEND
+
+      MockSchemaDumper.dump(connection, stream)
+      expect(expected_output.strip).to eq(stream.to_s)
+    end
+  end
+
+end
