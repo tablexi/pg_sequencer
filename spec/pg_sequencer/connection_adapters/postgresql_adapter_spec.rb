21a2410e90a3976347bd4abf63fe0ec64e0c714e
Ben Linton <ben.linton@tablexi.com>
2017-08-22 13:38:56 -0500

    Fix railtie and temporarily disable owned_by option

--- spec/pg_sequencer/connection_adapters/postgresql_adapter_spec.rb
+++ spec/pg_sequencer/connection_adapters/postgresql_adapter_spec.rb
@@ -10,13 +10,13 @@ describe PgSequencer::ConnectionAdapters::PostgreSQLAdapter do
       max: 2_000_000,
       cache: 5,
       cycle: true,
-      owned_by: "table_name.column_name",
+      # owned_by: "table_name.column_name",
     }
   end
 
   context "generating sequence option SQL" do
     it "includes all options" do
-      output = " INCREMENT BY 1 MINVALUE 1 MAXVALUE 2000000 START WITH 1 CACHE 5 CYCLE OWNED BY table_name.column_name"
+      output = " INCREMENT BY 1 MINVALUE 1 MAXVALUE 2000000 START WITH 1 CACHE 5 CYCLE"
       expect(dummy.sequence_options_sql(options.merge(start: 1))).to eq(output)
     end
 
@@ -96,12 +96,12 @@ describe PgSequencer::ConnectionAdapters::PostgreSQLAdapter do
     end
 
     context "for :owned_by" do
-      it "includes 'OWNED BY' in SQL if specified" do
+      xit "includes 'OWNED BY' in SQL if specified" do
         expect(dummy.sequence_options_sql(owned_by: "users.counter")).to eq(" OWNED BY users.counter")
         expect(dummy.sequence_options_sql(owned_by: "NONE")).to eq(" OWNED BY NONE")
       end
 
-      it "does not include 'OWNED BY' in SQL if specified as nil" do
+      xit "does not include 'OWNED BY' in SQL if specified as nil" do
         expect(dummy.sequence_options_sql(owned_by: nil)).to eq("")
       end
     end
@@ -117,7 +117,7 @@ describe PgSequencer::ConnectionAdapters::PostgreSQLAdapter do
 
     context "with options" do
       it "includes options at the end" do
-        output = "CREATE SEQUENCE things INCREMENT BY 1 MINVALUE 1 MAXVALUE 2000000 START WITH 1 CACHE 5 CYCLE OWNED BY table_name.column_name"
+        output = "CREATE SEQUENCE things INCREMENT BY 1 MINVALUE 1 MAXVALUE 2000000 START WITH 1 CACHE 5 CYCLE"
         expect(dummy.create_sequence_sql("things", options.merge(start: 1))).to eq(output)
       end
     end
@@ -134,7 +134,7 @@ describe PgSequencer::ConnectionAdapters::PostgreSQLAdapter do
 
     context "with options" do
       it "includes options at the end" do
-        output = "ALTER SEQUENCE things INCREMENT BY 1 MINVALUE 1 MAXVALUE 2000000 RESTART WITH 1 CACHE 5 CYCLE OWNED BY table_name.column_name"
+        output = "ALTER SEQUENCE things INCREMENT BY 1 MINVALUE 1 MAXVALUE 2000000 RESTART WITH 1 CACHE 5 CYCLE"
         expect(dummy.change_sequence_sql("things", options.merge(restart: 1))).to eq(output)
       end
     end
